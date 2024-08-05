fac9807ada00654701f3b5c1bdfe7895f3ea42dc
Troy <tydamon@hotmail.com>
2024-08-06 06:17:02 +1000

    basic starting files

---
+++ src/pages/About.jsx
@@ -0,0 +1,13 @@
+// src/pages/About.js
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>Learn more about us on this page.</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,13 @@
+// src/pages/Contact.js
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact Page</h1>
+      <p>Get in touch with us through this page.</p>
+    </div>
+  );
+};
+
+export default Contact;
